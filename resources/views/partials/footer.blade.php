395112cfcef75bbd8cf8bb398c7763d23276844d
EmanueleVenditti95 <vendittiema@gmail.com>
2023-12-11 16:44:36 +0100

    Add - footer (css to fix)

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,53 @@
+<footer class="page-footer" style="background-image: url( {{ Vite::asset('resources/images/footer-bg.jpg') }})">
+    <div class="container">
+        <div class="row">
+            <div class="col-6 d-flex">
+                <ol>
+                    <li class="list-title">Comics<a href=""></a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Comics</a></li>
+                </ol>
+            
+                <ol>
+                    <li class="list-title">Comics<a href=""></a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Comics</a></li>
+                </ol>
+            
+                <ol>
+                    <li class="list-title">Comics<a href=""></a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Comics</a></li>
+                </ol>
+            
+                <ol>
+                    <li class="list-title">Comics<a href=""></a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Comics</a></li>
+                </ol>
+            </div>
+            <div class="col-6" style="background-image: url( {{ Vite::asset('resources/images/dc-logo-bg.png') }})">
+            </div>
+        </div>
+    </div>
+    
+
+</footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<header class="page-header">
+<header class="page-header container">
 
     <div class="container">
         <nav class="row">
@@ -11,13 +11,13 @@
                     <li><a href="#">CHARACTERS</a></li>
                     <li><a href="#">COMICS</a></li>
                     <li><a href="#">MOVIES</a></li>
-                    <li><a href="#">TV</a></li>
+                    {{-- <li><a href="#">TV</a></li>
                     <li><a href="#">GAMES</a></li>
                     <li><a href="#">COLLECTIBLES</a></li>
                     <li><a href="#">VIDEOS</a></li>
                     <li><a href="#">FANS</a></li>
                     <li><a href="#">NEWS</a></li>
-                    <li><a href="#">SHOP</a></li>
+                    <li><a href="#">SHOP</a></li> --}}
                 </ol>
             </div>
 
